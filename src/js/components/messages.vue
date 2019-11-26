a8f0391befbe87d6570a311baafacf0bc5f4a72a
Arseniy.Zaoborniy <zabio90@gmail.com>
2019-11-26 17:24:53 +0300

    added create & delete message

--- src/js/components/messages.vue
+++ src/js/components/messages.vue
@@ -1,7 +1,13 @@
 <template>
   <div  class="default-text">
+    <div>
+      <input type="text" v-model="message" placeholder="type text" />
+      <button type="button" @click="handleSend">Send</button>
+    </div>
     <p v-for="message in messages" v-bind:key="message.id">
-      {{ message.id }}: {{ message.text }} from {{ message.creationDate }}
+      ({{ message.id }})
+      {{ message.text }}
+      <button type="button" @click="() => handleDelete(message.id)">X</button>
     </p>
   </div>
 </template>
@@ -11,12 +17,30 @@ export default {
   name: 'messages',
   data() {
     return {
+      message: null,
       messages: [],
     }
   },
+  methods: {
+    messagesFetch() {
+      this.$http.get('/api/message').then(result => this.messages = result.body);
+    },
+    handleSend() {
+      this.$http.post('/api/message', {text: this.message}).then(() => {
+        this.message = null;
+        this.messagesFetch();
+      });
+    },
+    handleDelete(id) {
+      this.$http.delete(`/api/message/${id}`).then(() => {
+        this.messagesFetch();
+      });
+    }
+  },
   created() {
-    this.$http.get('/api/message').then(result => this.messages = result.body);
-  }
+    this.messagesFetch();
+  },
+  
 }
 </script>
 
